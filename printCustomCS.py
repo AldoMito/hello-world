6c6a1214e35dd8344e8447f47592082616207dfa
AldoMito <aldomito@hotmail.com>
2016-02-10 12:14:15 -0800

    Create printCustomCS.py

---
+++ printCustomCS.py
@@ -0,0 +1,59 @@
+# p4PrintCScustom.py
+# Dylan House
+# Python Version 3.2.5.1
+# 2/3/2016
+# Print 'CS' in large, block letters based on user's desired character.
+
+userWhim = input("Please enter your character of choice: ")
+x = userWhim
+
+print ('********************************\n\n')
+print('     %s%s%s%s%s%s         %s%s%s%s' %(x,x,x,x,x,x,x,x,x,x))
+print ('    %s      %s       %s    %s' %(x,x,x,x))
+print ('    %s       %s     %s      %s ' %(x,x,x,x))
+print ('    %s       %s    %s' %(x,x,x))
+print ('    %s            %s' %(x,x))
+print ('    %s             %s' %(x,x))
+print ('    %s              %s' %(x,x))
+print ('    %s               %s' %(x,x))
+print ('    %s                %s%s%s' %(x,x,x,x))
+print ('    %s                   %s' %(x,x))
+print ('    %s                    %s' %(x,x))
+print ('    %s                     %s' %(x,x))
+print ('    %s            %s        %s' %(x,x,x))
+print ('    %s       %s    %s%s      %s' %(x,x,x,x,x))
+print ('    %s       %s      %s%s   %s' %(x,x,x,x,x))
+print ('     %s%s%s%s%s%s%s        %s%s%s%s\n\n' %(x,x,x,x,x,x,x,x,x,x,x))
+print ('********************************\n')
+print ('           Computer Science is cool stuff!')
+
+'''
+>>> ================================ RESTART ================================
+>>> 
+Please enter your character of choice: X
+********************************
+
+
+     XXXXXX         XXXX
+    X      X       X    X
+    X       X     X      X 
+    X       X    X
+    X            X
+    X             X
+    X              X
+    X               X
+    X                XXX
+    X                   X
+    X                    X
+    X                     X
+    X            X        X
+    X       X    XX      X
+    X       X      XX   X
+     XXXXXXX        XXXX
+
+
+********************************
+
+           Computer Science is cool stuff!
+>>> 
+'''
